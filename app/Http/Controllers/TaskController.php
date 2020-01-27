51e9fcfa220c690e1ab12e1e6ccd092baec8b9c8
MasatoKotani629 <masatowww@gmail.com>
2020-01-28 00:08:52 +0900

    タスクの編集機能を追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Folder;
 use App\Task;
 use App\http\Requests\CreateTask;
+use App\http\Requests\EditTask;
 
 class TaskController extends Controller
 {
@@ -50,6 +51,33 @@ class TaskController extends Controller
             'id' => $current_folder->id,
         ]);
 
+    }
+
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task  = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+
+    }
+
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+        // 1
+        $task = Task::find($task_id);
+
+        // 2
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        // 3
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
+        ]);
     }
 
 }
